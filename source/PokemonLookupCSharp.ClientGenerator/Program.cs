1dd9d26000f7199ebbb22a36d5e4349a0483c29c
Zach Esposito <zach@zachesposito.me>
2019-02-10 22:22:43 -0500

    Initial commit, gets random pokemon

---
+++ source/PokemonLookupCSharp.ClientGenerator/Program.cs
@@ -0,0 +1,36 @@
+﻿using NSwag;
+using NSwag.CodeGeneration.CSharp;
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace PokemonLookupCSharp.ClientGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerateClient().GetAwaiter().GetResult();
+        }
+
+        static async Task GenerateClient()
+        {
+            var document = await SwaggerDocument.FromUrlAsync("http://localhost:58829/swagger/v1/swagger.json");
+
+            var settings = new SwaggerToCSharpClientGeneratorSettings
+            {
+                ClassName = "PokemonLookupAPIClient",
+                CSharpGeneratorSettings =
+                {
+                    Namespace = "PokemonLookupCSharp"
+                }
+            };
+
+            var generator = new SwaggerToCSharpClientGenerator(document, settings);
+            var code = generator.GenerateFile(NSwag.CodeGeneration.ClientGeneratorOutputType.Full);
+            var appFolder = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.FullName, "PokemonLookupCSharp");
+            File.WriteAllText(Path.Combine(appFolder, "Client.cs"), code);
+        }
+    }
+}
---
+++ source/PokemonLookupCSharp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokemonLookupCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Pokemon Lookup!");
+            Console.WriteLine("Your random Pokemon is: ");
+            var randomPokemon = GetRandomPokemon().GetAwaiter().GetResult();
+            PrintPokemon(randomPokemon);
+
+            Console.WriteLine(Environment.NewLine + "Press enter to close.");
+            Console.ReadLine();
+        }
+
+        private static void PrintPokemon(Pokemon pokemon)
+        {
+            Console.WriteLine($"* * * {pokemon.Name} * * *");
+            Console.WriteLine($"ID: {pokemon.Id.ToString()}");
+            Console.WriteLine($"Species: {pokemon.Species.Name}");
+            Console.WriteLine($"Types: {pokemon.Types.Select(t => t.Type.Name).Aggregate((built, next) => built + ", " + next) }");
+            Console.WriteLine($"Stats:");
+            foreach (var stat in pokemon.Stats)
+            {
+                Console.WriteLine($"  {stat.Stat1.Name}: {stat.Base_stat}");
+            }
+        }
+
+        public static async Task<Pokemon> GetRandomPokemon()
+        {
+            var client = new PokemonLookupAPIClient("http://localhost:58829", new System.Net.Http.HttpClient());
+            var names = await client.GetPokemonIndexAsync();
+            var randomPokemon = await client.GetSpecificPokemonAsync(names.ToArray()[new Random().Next(0, names.Count - 1)]);
+            return randomPokemon;
+        }
+    }
+}
